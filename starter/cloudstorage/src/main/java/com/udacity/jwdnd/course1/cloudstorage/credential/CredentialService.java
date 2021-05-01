d5d36d70747a9f5a01f665e3c60c7fdbf4e48685
Gustavo Antonio Lastra Colorado <gustavo.lastra@iteratec.com>
2021-05-01 15:49:41 +0200

    add show decrypted passwords on modal dialog

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/credential/CredentialService.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/credential/CredentialService.java
@@ -1,35 +1,49 @@
 package com.udacity.jwdnd.course1.cloudstorage.credential;
 
+import com.udacity.jwdnd.course1.cloudstorage.auth.EncryptionService;
 import com.udacity.jwdnd.course1.cloudstorage.auth.HashService;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.security.SecureRandom;
+import java.util.Base64;
 import java.util.List;
 
 @Service
 public class CredentialService {
     private final CredentialMapper credentialMapper;
     private final HashService hashService;
+    private final EncryptionService encryptionService;
 
-    public CredentialService(CredentialMapper credentialMapper, HashService hashService) {
+    public CredentialService(CredentialMapper credentialMapper, HashService hashService, EncryptionService encryptionService) {
         this.credentialMapper = credentialMapper;
         this.hashService = hashService;
+        this.encryptionService = encryptionService;
     }
 
     public boolean saveCredential(Credential credential) throws IOException {
-        String encodedSalt = hashService.getEncodedSalt();
-        credential.setKey(encodedSalt);
-        credential.setPassword(hashService.getHashedValue(credential.getPassword(), encodedSalt));
-
+        SecureRandom random = new SecureRandom();
+        byte[] key = new byte[16];
+        random.nextBytes(key);
+        String encodedKey = Base64.getEncoder().encodeToString(key);
+        String encryptedPassword = encryptionService.encryptValue(credential.getPassword(), encodedKey);
+        credential.setKey(encodedKey);
+        credential.setPassword(encryptedPassword);
         if (credential.getCredentialId() != null && credentialMapper.getCredential(credential) != null) {
             return credentialMapper.updateCredential(credential);
         }
-
         return credentialMapper.saveCredential(credential);
     }
 
     public List<Credential> getCredentialList(Integer userId) {
-        return credentialMapper.getCredentialList(userId);
+        List<Credential> credentialList = credentialMapper.getCredentialList(userId);
+        if (!credentialList.isEmpty()) {
+            for (Credential credential : credentialList) {
+                credential.setPassword(encryptionService.decryptValue(credential.getPassword(), credential.getKey()));
+            }
+        }
+
+        return credentialList;
     }
 
     public boolean deleteCredential(Integer credentialId, Integer userId) {
@@ -37,9 +51,9 @@ public class CredentialService {
     }
 
     public boolean updateCredential(Credential credential, Integer userId) {
-        String encodedSalt = hashService.getEncodedSalt();
-        credential.setKey(encodedSalt);
-        credential.setPassword(hashService.getHashedValue(credential.getPassword(), encodedSalt));
+        Credential cred = credentialMapper.getCredential(credential);
+        credential.setKey(cred.getKey());
+        credential.setPassword(encryptionService.encryptValue(credential.getPassword(), credential.getKey()));
         return credentialMapper.updateCredential(credential);
     }
 }
